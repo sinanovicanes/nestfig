a1e1de9a26fb871dce2871c29c406758543f84bf
sinanovicanes <sinanovicanes01@gmail.com>
2024-09-30 21:18:33 +0300

    feat: Update ConfigResolver to load config from different files

--- lib/resolvers/config.resolver.ts
+++ lib/resolvers/config.resolver.ts
@@ -1,11 +1,15 @@
+import { Logger } from "@nestjs/common";
 import { plainToInstance } from "class-transformer";
 import { validateSync } from "class-validator";
+import { existsSync, readFileSync } from "fs";
+import { CONFIG_FIELDS_TOKEN, CONFIG_TOKEN } from "../constants";
 import { FieldMetadata } from "../decorators";
 import { ConfigOptions } from "../interfaces";
 import { Config } from "../types";
-import { CONFIG_TOKEN, CONFIG_FIELDS_TOKEN } from "../constants";
 
 export class ConfigResolver {
+  private static logger = new Logger(ConfigResolver.name);
+
   private static validateConfig<T extends Function>(config: Config<T>, instance: T) {
     const validatedConfig = plainToInstance(config, instance, {
       enableImplicitConversion: true
@@ -22,11 +26,36 @@ export class ConfigResolver {
     return validatedConfig;
   }
 
+  private static loadConfigFile(path: string): Record<string, any> {
+    if (!existsSync(path)) {
+      throw new Error(`Config file not found at ${path}`);
+    }
+
+    const pathExt = path.split(".").pop().toLowerCase();
+
+    switch (pathExt) {
+      case "json":
+        try {
+          return JSON.parse(readFileSync(path, "utf-8"));
+        } catch (e) {
+          throw new Error(`Failed to parse JSON config file at ${path}: ${e}`);
+        }
+      // TODO: Add support for other file types like yaml, toml, etc.
+      default:
+        this.logger.warn(`Unsupported file extension ${pathExt}`);
+        break;
+    }
+  }
+
   private static getConfigData(options: ConfigOptions) {
     const config: Record<string, any> = {};
 
     if (options.paths) {
-      // TODO: Load config from paths
+      options.paths.forEach(path => {
+        const data = this.loadConfigFile(path);
+
+        Object.assign(config, data);
+      });
     }
 
     if (!options.ignoreEnv) {
